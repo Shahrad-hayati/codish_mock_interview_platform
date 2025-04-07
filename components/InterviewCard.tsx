a82a4699372009ee55b8f5d93d46edc8026151a4
Shahrad Hayati <shahradhayati@yahoo.com>
2025-04-07 23:20:30 +0330

    implement interview session

--- components/InterviewCard.tsx
+++ components/InterviewCard.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 import { getRandomInterviewCover } from "@/lib/utils";
 import DisplayTechIcons from "./DisplayTechIcons";
 const InterviewCard = ({
-  interviewId,
+  id,
   userId,
   role,
   type,
@@ -62,11 +62,7 @@ const InterviewCard = ({
           <DisplayTechIcons techStack={techstack} />
           <Button className="btn-primary">
             <Link
-              href={
-                feedback
-                  ? `/interview/${interviewId}/feedback`
-                  : `/interview/${interviewId}`
-              }
+              href={feedback ? `/interview/${id}/feedback` : `/interview/${id}`}
             >
               {feedback ? "Check Feedback" : "View Interview"}
             </Link>
